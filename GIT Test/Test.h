7040a338342b268a9a6811045fe6a9b8b6195805
Garrett Gitterman <garrettgitterman@garrettrmansMBP.home>
2014-11-05 18:42:49 -0500

    added a test class

---
+++ GIT Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GIT Test
+//
+//  Created by Garrett Gitterman on 11/5/14.
+//  Copyright (c) 2014 Garrett Gitterman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GIT Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GIT Test
+//
+//  Created by Garrett Gitterman on 11/5/14.
+//  Copyright (c) 2014 Garrett Gitterman. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
