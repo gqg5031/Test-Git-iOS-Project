92b99a86e72158e0eadf2ed0ea7d5af172db0582
Garrett Gitterman <garrettgitterman@garrettrmansMBP.home>
2014-11-05 18:31:00 -0500

    Initial Commit

---
+++ GIT Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT Test
+//
+//  Created by Garrett Gitterman on 11/5/14.
+//  Copyright (c) 2014 Garrett Gitterman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIT Test
+//
+//  Created by Garrett Gitterman on 11/5/14.
+//  Copyright (c) 2014 Garrett Gitterman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
